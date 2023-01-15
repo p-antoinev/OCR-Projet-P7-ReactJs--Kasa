981b6a80bb2c384ba6324ef7f6c0f5434387215c
p-antoinev <pavrnsrf@gmail.com>
2023-01-15 10:07:04 +0100

    indentation du code

--- src/components/HousingList/index.jsx
+++ src/components/HousingList/index.jsx
@@ -7,18 +7,16 @@ import { Link } from 'react-router-dom'
 //Composants
 import Card from '../Card'
 
-
-
 function HousingList() {
-    return(
-        <div className='card--background'>
-                {Data.map((item) => (
-                    <Link key={item.id} className='card--link' to={`/housing/${item.id}`}>
-                        <Card  title={item.title} cover={item.cover}   /> 
-                   </Link>
-                ))}
-        </div>
-    )
+  return (
+    <div className="card--background">
+      {Data.map((item) => (
+        <Link key={item.id} className="card--link" to={`/housing/${item.id}`}>
+          <Card title={item.title} cover={item.cover} />
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 // export to |-src|-pages|-Home
